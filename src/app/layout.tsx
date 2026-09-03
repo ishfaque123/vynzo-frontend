68365018faa403170c02ddb842e45d1ca3bf0674
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-03 23:38:40 +0500

    Wire up Shell layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Shell from '@/components/Shell';
 
 export const metadata = {
   title: 'Vynzo',
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Shell>{children}</Shell>
+      </body>
     </html>
   );
 }
