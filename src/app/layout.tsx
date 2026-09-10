df71f0debad7448864307e7a6e570193688f3bd7
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-10 18:43:47 +0500

    feat: add Google AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 import Shell from '@/components/Shell';
 import ThemeProvider from '@/components/ThemeProvider';
 
@@ -10,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8689135580842158"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         <ThemeProvider>
           <Shell>{children}</Shell>
