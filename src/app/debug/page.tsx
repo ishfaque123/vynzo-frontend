6c08751c630dedfc1b0efd925a3b5bb958bed18c
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-14 12:43:12 +0500

    chore: remove leftover debug page

--- src/app/debug/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DebugPage() {
-  return (
-    <div style={{ padding: 20, fontFamily: 'monospace', fontSize: 16 }}>
-      <p>NEXT_PUBLIC_API_URL = {process.env.NEXT_PUBLIC_API_URL || '(EMPTY)'}</p>
-    </div>
-  );
-}
